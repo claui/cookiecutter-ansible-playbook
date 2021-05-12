8a19523441807e67ea572ffc96f83825766980af
Claudia <claui@users.noreply.github.com>
2021-05-12 21:38:44 +0200

    Add optional support for Ansible 2.5

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,14 @@
+"""
+{%
+    set version_tuple_dict = {
+        "Ansible 2.5 or higher": (2, 5),
+        "Ansible 2.10 or higher": (2, 10),
+    }
+%}
+{{
+    cookiecutter.update({
+        "ansible_version_tuple_min_required":
+            version_tuple_dict[cookiecutter.ansible_version],
+    })
+}}
+"""
